366b1166e803a0ac577d12393c0305da5b45d361
ivnicolas <ikennanicolas@gmail.com>
2021-05-31 22:54:45 -0400

    commit tree c006ee728b794bc571dd15a6efb0d34623688a63

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching(drivers,name){
+   return drivers.filter( n => n.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers,letters){
+    return drivers.filter( n => n.substring(0,2).toLowerCase() === letters.toLowerCase())
+}
+
+function matchName(drivers,name){
+    return drivers.filter(n => n.name.toLowerCase() === name.toLowerCase())
+}
